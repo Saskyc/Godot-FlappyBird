3b83c452487852c32c7370823bbfd703ab577d9c
Saskyc <kokoskorp@gmail.com>
2026-06-03 21:29:25 +0200

    Basically nothing yet

---
+++ flappy-bird/BirdScript.cs
@@ -0,0 +1,42 @@
+using Godot;
+
+namespace FlappyBird;
+
+public partial class BirdScript : Sprite2D
+{
+	[Export]
+	public Camera2D Camera;
+
+	[Export]
+	public RigidBody2D Rigid;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	public float Time = 0;
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.ToString().Contains("Motion")) return;
+		GD.Print(@event.GetType().Name);
+		if (@event is InputEventMouseButton mouseButton)
+		{
+			Rigid.ApplyForce(new Vector2(0, 1).Normalized());
+		}
+		base._Input(@event);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Camera.Position = new Vector2(Position.X, Position.Y);
+		GD.Print(Position);
+	}
+
+	public bool Jumping = false;
+
+	public Vector2 Peak = Vector2.Zero;
+	public Vector2 Original = Vector2.Zero;
+}
---
+++ flappy-bird/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using Godot;
+
+namespace FlappyBird;
+
+public partial class TestScript : Label
+{
+    public static float C = 0;
+    
+    public override void _Ready()
+    {
+        C += 0.5f;
+        GD.Print("Started Test script");
+        Text = C.ToString(CultureInfo.InvariantCulture);
+        Position = new Vector2(C, C);
+    }
+}
